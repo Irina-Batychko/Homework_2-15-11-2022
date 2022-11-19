4abea360b185972724e277a3b9d32a1511663900
IRINA BATYCHKO <mironenkoi@yandex.ru>
2022-11-20 00:20:35 +0300

    added tasks

---
+++ Task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 13:
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    String str = number.ToString();
+    Console.WriteLine(str[2]);
+}
+
+
